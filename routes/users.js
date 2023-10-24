ef58b8643bb4c5eaefec22f7f9154b4ffcdbe683
Pénélope Grunwald <penelope.grunwald@gmail.com>
2023-10-24 16:32:50 +0200

    petite modif route signin

--- routes/users.js
+++ routes/users.js
@@ -48,7 +48,7 @@ router.post('/signin', (req, res) => {
 
   User.findOne({ username: req.body.username }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, token: data.token, firstname:data.firstname});
+      res.json({ result: true, token: data.token, firstname: data.firstname, email: data.email, lastname: data.lastname});
     } else {
       res.json({ result: false, error: 'User not found or wrong password' });
     }
